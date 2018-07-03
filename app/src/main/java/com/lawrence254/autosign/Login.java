fca0d4effa1e187b1dde80016a51b88bc2001c96
Lawrence <lawrenslava1@gmail.com>
2018-07-03 10:27:14 +0300

    Delete the Login class and recreate it as an activity

--- app/src/main/java/com/lawrence254/autosign/Login.java
+++ app/src/main/java/com/lawrence254/autosign/Login.java
@@ -1,4 +1,13 @@
 package com.lawrence254.autosign;
 
-public class Login {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
