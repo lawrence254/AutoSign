daf360c52d18725fa8d20240956f6b6850a584a0
Lawrence <lawrenslava1@gmail.com>
2018-07-03 08:51:36 +0300

    Change version of nearby connections api

--- app/src/main/java/com/lawrence254/autosign/MainActivity.java
+++ app/src/main/java/com/lawrence254/autosign/MainActivity.java
@@ -1,13 +1,62 @@
 package com.lawrence254.autosign;
 
+import android.bluetooth.le.AdvertisingSetCallback;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.AdvertisingOptions;
+import com.google.android.gms.nearby.connection.ConnectionInfo;
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
+import com.google.android.gms.nearby.connection.ConnectionResolution;
+import com.google.android.gms.nearby.connection.Connections;
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
+import com.google.android.gms.nearby.connection.DiscoveryOptions;
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
+import com.google.android.gms.nearby.messages.Strategy;
 
+public class MainActivity extends AppCompatActivity  implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+
+    public static final String CLIENT_NAME="TEACHER";
+    public static final String SERVICE_ID="MC9-ANDROID";
+    public static final String STRATEGY= "P2P_STAR";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleApiClient mGoogleApiClient = new GoogleApiClient
+                .Builder(this, this, this)
+                .addApi(Nearby.CONNECTIONS_API)
+                .enableAutoManage(this, this)
+                .build();
+
     }
-}
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        startAdvertising();
+    }
+
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+
+ }
