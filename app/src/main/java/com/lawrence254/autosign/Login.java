74e0aaca1464569525cfbebe9eee60f01231c4d5
Lawrence <lawrenslava1@gmail.com>
2018-07-03 15:57:31 +0300

    Add companion application for students(Reciever Application)

--- app/src/main/java/com/lawrence254/autosign/Login.java
+++ app/src/main/java/com/lawrence254/autosign/Login.java
@@ -1,12 +1,21 @@
 package com.lawrence254.autosign;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.lawrence254.autosign.client.ClientActivity;
@@ -15,55 +24,70 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class Login extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
     private static final int RC_SIGN_IN = 123;
+    @BindView(R.id.email)EditText mEmail;
+    @BindView(R.id.pass)EditText mPass;
+    @BindView(R.id.login)Button mlogin;
+    FirebaseAuth auth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        auth = FirebaseAuth.getInstance();
 
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(Collections.singletonList(new AuthUI.IdpConfig.EmailBuilder().build()))
-                        .build(),
-                RC_SIGN_IN);
+        if (auth.getCurrentUser() !=null){
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        }
+
+        mlogin.setOnClickListener(this);
     }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    public void onClick(View v) {
+        if (v==mlogin){
+            String email = mEmail.getText().toString();
+            final String password = mPass.getText().toString();
 
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-
-            if (resultCode == RESULT_OK) {
-                // Successfully signed in
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-                if() {
-                    savedItems = sharedpreferences.getString(MyPREFERENCES.toString(), "");
-                    selectedItems.addAll(Arrays.asList(savedItems.split(",")));
-
-                }
-
-                Toast.makeText(this, "Welcome to StockTrack "+user.getDisplayName()+". Your news sources: "+savedItems, Toast.LENGTH_LONG).show();
-                if(sharedpreferences.contains(MyPREFERENCES)){
-
-                    Intent intent = new Intent(this,MainActivity.class);
-                    startActivity(intent);
-                }else {
-                    Intent intent = new Intent(this,ClientActivity.class);
-                    startActivity(intent);
-                }
-                // ...
-            } else {
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                // ...
-                response.getError().getErrorCode();
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                return;
             }
+
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            progressBar.setVisibility(View.VISIBLE);
+
+            auth.signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressBar.setVisibility(View.GONE);
+                            if (!task.isSuccessful()) {
+                                // there was an error
+                                if (password.length() < 6) {
+                                    mPass.setError(getString(R.string.minimum));
+                                } else {
+                                    Toast.makeText(Login.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                                }
+                            } else {
+                                Intent intent = new Intent(Login.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+
+                        }
+                    });
         }
     }
 }
--- app/src/main/java/com/lawrence254/autosign/RegisterActivity.java
+++ app/src/main/java/com/lawrence254/autosign/RegisterActivity.java
@@ -8,13 +8,19 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.data.model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,7 +30,6 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     @BindView(R.id.uPass)EditText mPass;
     @BindView(R.id.uregister)Button mRegister;
     FirebaseAuth auth;
-    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,12 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         setContentView(R.layout.activity_register);
         ButterKnife.bind(this);
         auth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = auth.getCurrentUser();
+        if (user != null){
+            Intent intent =  new Intent(RegisterActivity.this, Login.class);
+            startActivity(intent);
+        }
 
         mRegister.setOnClickListener(this);
     }
@@ -56,29 +67,33 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                 Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
                 return;
             }
-
-            progressBar.setVisibility(View.VISIBLE);
-            //create user
+            
             auth.createUserWithEmailAndPassword(email, pass)
                     .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
-                            Toast.makeText(RegisterActivity.this, "Account created" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
-                            progressBar.setVisibility(View.GONE);
-                            if (!task.isSuccessful()) {
-                                Toast.makeText(RegisterActivity.this, "Account creation failed." + task.getException(),
-                                        Toast.LENGTH_SHORT).show();
-                            } else {
-                                startActivity(new Intent(RegisterActivity.this, Login.class));
-                                finish();
-                            }
+
+                            FirebaseUser user = auth.getCurrentUser();
+                            insertData(user);
+//                            Toast.makeText(RegisterActivity.this, "User data: "+user.getEmail(), Toast.LENGTH_SHORT).show();
                         }
                     });
         }
     }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        progressBar.setVisibility(View.GONE);
+
+    private void insertData(FirebaseUser user) {
+        if (user != null){
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+            Map<String, String> map = new HashMap<>();
+            map.put("email", user.getProviders().get(0));
+//            if(user.getProviderData().contains(0)) {
+//                map.put("email", user.getProviderData().get(0).toString());
+//            }
+            Toast.makeText(this, "Regg"+map, Toast.LENGTH_SHORT).show();
+            ref.child("students").child(user.getUid()).setValue(map);
+            Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
